b189ccbe09dbf9014fc80cb2ed8f443ca0640ff1
hdriqi <riqi.asdf@gmail.com>
2020-07-28 11:21:58 +0700

    add web3

--- components/Layout/dashboard.js
+++ components/Layout/dashboard.js
@@ -1,12 +1,32 @@
 import Link from "next/link";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 
 import Nav from "../Navigation/Nav";
 import Footer from "../Navigation/Footer";
 import SideNav from "../Navigation/SideNav";
 
+import { getWeb3 } from "@/utils/index"
+
 function PageLayout({ children }) {
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      web3.eth.handleRevert = true
+
+      // Use web3 to get the user's accounts.
+      try {
+        const accounts = await web3.eth.getAccounts();
+        // logged in
+        console.log(accounts)
+      } catch (err) {
+        // not logged in
+        console.log(err)
+      }
+    }
+    init()
+  })
+
   return (
     <div className="flex flex-col min-h-screen h-full bg-gray-300">
       <Nav />
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,6 @@
+import getWeb3 from "./getWeb3";
+
 export { get } from "./get";
 export { formatDate } from "./formatDate";
 export { lockScroll } from "./lockScroll";
+export { getWeb3 } from "./getWeb3.js";
