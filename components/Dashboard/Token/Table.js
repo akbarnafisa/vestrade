4e6c71aa85d19f970c5914e9e56a7f7e8a349005
hdriqi <riqi.asdf@gmail.com>
2020-07-28 12:33:44 +0700

    add totalSupply to token list

--- components/Dashboard/Token/Table.js
+++ components/Dashboard/Token/Table.js
@@ -1,5 +1,48 @@
-import React from "react";
-import { get } from "@/utils/index";
+import React, { useEffect, useState } from "react";
+import { get, prettyBalance } from "@/utils/index";
+import { useEth } from "@/components/Layout/dashboard";
+
+import VestradeERC20 from '@/contracts/Vestrade_ERC20.json'
+
+const TokenRow = ({token, index, setModalEditToken}) => {
+  const { web3 } = useEth()
+
+  const [totalSupply, setTotalSupply] = useState(null)
+
+  useEffect(() => {
+    const getTotalSupply = async () => {
+      const tokenContract = new web3.eth.Contract(
+        VestradeERC20.abi,
+        token.tokenAddr
+      )
+      const totalSupply = await tokenContract.methods.totalSupply().call()
+      setTotalSupply(prettyBalance(totalSupply))
+    }
+    if (web3 && token) {
+      getTotalSupply()
+    }
+  }, [web3, token])
+
+  return (
+    <tr className={index % 2 !== 0 ? `bg-gray-200` : ``} key={index}>
+      <td className="border-t px-6 py-4">
+        {get(token, `symbol`, `-`)}
+      </td>
+      <td className="border-t px-6 py-4">
+        {get(token, `name`, `-`)}
+      </td>
+      <td className="border-t font-bold px-6 py-4">{totalSupply}</td>
+      <td className="border-t">
+        <div
+          className="w-6 h-6 cursor-pointer"
+          onClick={() => setModalEditToken(token)}
+        >
+          <img className="w-4 h-auto" src="/icon/edit.svg" />
+        </div>
+      </td>
+    </tr>
+  )
+}
 
 export default ({ tokens, setModalEditToken }) => {
   return (
@@ -16,23 +59,7 @@ export default ({ tokens, setModalEditToken }) => {
         <tbody>
           {tokens.map((token, index) => {
             return (
-              <tr className={index % 2 !== 0 ? `bg-gray-200` : ``} key={index}>
-                <td className="border-t px-6 py-4">
-                  {get(token, `symbol`, `-`)}
-                </td>
-                <td className="border-t px-6 py-4">
-                  {get(token, `name`, `-`)}
-                </td>
-                <td className="border-t font-bold px-6 py-4">0.000</td>
-                <td className="border-t">
-                  <div
-                    className="w-6 h-6 cursor-pointer"
-                    onClick={() => setModalEditToken(token)}
-                  >
-                    <img className="w-4 h-auto" src="/icon/edit.svg" />
-                  </div>
-                </td>
-              </tr>
+              <TokenRow token={token} index={index} setModalEditToken={setModalEditToken} />
             );
           })}
         </tbody>
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,8 @@
 import getWeb3 from "./getWeb3";
+import { prettyBalance } from "./prettyBalance";
 
 export { get } from "./get";
 export { formatDate } from "./formatDate";
 export { lockScroll } from "./lockScroll";
-export { getWeb3 } from "./getWeb3.js";
+export { getWeb3 } from "./getWeb3";
+export { prettyBalance } from './prettyBalance';
